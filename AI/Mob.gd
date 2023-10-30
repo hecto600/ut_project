5633e99b6fcc2df685fb69d3e7bf6f4984f6a559
Hector Pinheiro <hecto600@gmail.com>
2023-10-30 11:34:39 -0300

    Fixed state transition conflicts with vision and noise detection; Changed player boost mechanics; Add sprite animation for player

--- AI/Mob.gd
+++ AI/Mob.gd
@@ -8,6 +8,7 @@ extends CharacterBody2D
 @onready var timer_undetected: Timer = $Pivot/TimerUndetected
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var hud: Control = $CanvasLayer/HUD
+@onready var hearing_shape: CollisionShape2D = $Pivot/DetectionArea/HearingDetectionShape
 
 @export var speed: float = 350.0
 @export_range(0.01, 1.0, 0.01) var drag_factor: float = 0.2
@@ -31,10 +32,12 @@ enum State {
 }
 
 var found_player: bool = false
+var heard_player: bool = false
 var in_noise_range: bool = false
+
 var timer_to_detect: float = 0.75
 var timer_to_detect_sound: float = 2.0
-var timer_to_alert: float = 3.5
+var timer_to_attack: float = 5.0
 var timer_alert_to_patrol: float = 8.0
 
 func _ready() -> void:
@@ -44,6 +47,8 @@ func _ready() -> void:
 	detection_sound_area.body_exited.connect(_on_detection_sound_area_body_exited)
 	_create_rays()
 
+func _draw():
+	draw_circle(Vector2.ZERO, hearing_shape.get_shape().radius, Color(Color.AQUA, 0.2))
 
 func _create_rays() -> void:
 	for ray_offset in range(-rays_amount / 2 , rays_amount / 2 + 1 ):
@@ -64,18 +69,38 @@ func vision_detection() -> void:
 			break
 
 
+func noise_detection() -> void:
+	heard_player = false
+	if target != null and target.speed == target.speed_boost and in_noise_range:
+		heard_player = true
+		_pivot.look_at(target.global_position)
+		coll_shape.look_at(target.global_position)
+		
+		if curr_state == State.STATE_IDLE or curr_state == State.STATE_PATROL:
+			curr_state = State.STATE_SEARCH
+			animation_player.pause()
+			timer_detected.start(timer_to_detect)
+			
+		elif curr_state == State.STATE_ALERT:
+			curr_state = State.STATE_ATTACK
+			animation_player.pause()
+			timer_detected.start(timer_to_attack)
+
+
 func state_machine():
 	vision_detection()
+	noise_detection()
+	
+	print(curr_state)
 		
 	match curr_state:
 		State.STATE_IDLE:
-			if found_player:
+			if found_player or heard_player:
 				curr_state = State.STATE_SEARCH
 				timer_detected.start(timer_to_detect)
 			else:
 				cov.color = Color(0.0, 1.0, 0.0, 0.5)
-				hud.visible = false
-			
+				
 		State.STATE_SEARCH:
 			hud.visible = true
 			hud.get_node("Panel/VBox/State mode").text = "Search mode"
@@ -83,25 +108,21 @@ func state_machine():
 			
 			cov.color = Color(1.0, 1.0, 0.0, 0.5)
 			
-			
 		State.STATE_ATTACK:
 			cov.color = Color(1.0, 0.0, 0.0, 0.5)
 			
-			if found_player:
-				hud.get_node("Panel/VBox/State mode").text = "Attack mode"
-				timer_detected.start(timer_to_alert) # time restarted
+			if found_player or heard_player:
+				timer_detected.start(timer_to_attack) # time restarted
 				_set_has_target()
-			
-			if found_player:
+				hud.get_node("Panel/VBox/State mode").text = "Attack mode"
 				hud.get_node("Panel/VBox/State timer").text = str(timer_detected.wait_time).pad_decimals(2).pad_zeros(2)
+
 			else:
 				hud.get_node("Panel/VBox/State timer").text = str(timer_detected.time_left).pad_decimals(2).pad_zeros(2)
-				
-			
 		
 		State.STATE_ALERT:
 			cov.color = Color(1.0, 1.0, 0.0, 0.5)
-			if found_player:
+			if found_player or heard_player:
 				animation_player.pause()
 				timer_undetected.stop()
 				curr_state = State.STATE_ATTACK
@@ -109,10 +130,10 @@ func state_machine():
 			hud.get_node("Panel/VBox/State timer").text = str(timer_undetected.time_left).pad_decimals(2).pad_zeros(2)
 		
 		State.STATE_PATROL:
-			hud.visible = false
+			
 			cov.color = Color(0.0, 1.0, 0.0, 0.5)
 			
-			if found_player:
+			if found_player or heard_player:
 				curr_state = State.STATE_SEARCH
 				timer_detected.start(timer_to_detect)
 				animation_player.pause()
@@ -121,10 +142,10 @@ func state_machine():
 
 
 func _on_timer_detected_timeout() -> void:
-	if found_player and curr_state == State.STATE_SEARCH:
+	if found_player or heard_player and curr_state == State.STATE_SEARCH:
 		already_detected = true
 		curr_state = State.STATE_ATTACK
-		timer_detected.start(timer_to_alert)
+		timer_detected.start(timer_to_attack)
 		hud.get_node("Panel/VBox/State mode").text = "Attack mode"
 		
 	elif curr_state == State.STATE_ATTACK:
@@ -132,59 +153,35 @@ func _on_timer_detected_timeout() -> void:
 		timer_undetected.start(timer_alert_to_patrol)
 		animation_player.play("state_alert")
 		hud.get_node("Panel/VBox/State mode").text = "Alert mode"
-		target = null
 		
 	elif not already_detected:
 		curr_state = State.STATE_IDLE
-		target = null
-		
+		hud.visible = false
 	elif already_detected:
 		curr_state = State.STATE_PATROL
 		animation_player.play("state_patrol")
-		
-	else:
-		print("Shouldn't exist")
+		hud.visible = false
+
 
 func _on_timer_undetected_timeout() -> void:
-	
 	curr_state = State.STATE_PATROL
 	animation_player.play("state_patrol")
+	hud.visible = false
 
 
 func _on_detection_sound_area_body_entered(body: CharacterBody2D) -> void:
-	if body is Player and body.speed == body.speed_boost:
-		curr_state = State.STATE_SEARCH
-		in_noise_range = true
-		found_player = true
+	in_noise_range = true
+	if body is Player:
 		target = body
-		_pivot.look_at(body.global_position)
-		coll_shape.look_at(body.global_position)
-		timer_detected.start(timer_to_detect_sound)
 
 
 func _on_detection_sound_area_body_exited(body: CharacterBody2D) -> void:
-	
-	if body is Player:
-		curr_state = State.STATE_IDLE
-		timer_detected.stop()
-		target = null
-		
-	elif body is Player and already_detected:
-		curr_state = State.STATE_PATROL
-		in_noise_range = false
-		found_player = false
-		timer_detected.stop()
-		target = null
-		
-func _process(delta):
-	pass
+	in_noise_range = false
+
 
 func _physics_process(_delta: float) -> void: 
 	state_machine()
-	if Input.is_action_just_pressed("move_boost") and in_noise_range:
-		print("why not??")
-		curr_state = State.STATE_SEARCH
-	
+
 
 func _set_has_target() -> void:
 	var direction: Vector2 = Vector2.ZERO
--- player/Player.gd
+++ player/Player.gd
@@ -13,16 +13,22 @@ extends CharacterBody2D
 		speed = value
 		
 @export_range(0.01, 1.0, 0.01) var drag_factor: float = 0.12
-@export var value_boost: float = 4.0
-@export var value_cooldown: float = 10.0
+@export var time_boost: float = 4.0
+@export var time_cooldown: float = 5.0
 
 @onready var timer_boost: Timer = $TimerBoost
 @onready var timer_cooldown: Timer = $TimerCooldown
 @onready var ui_speed: Label = $CanvasLayer/Panel/HBoxContainer/VBoxContainer/LabelSpeed
 @onready var ui_timeleft: Label = $CanvasLayer/Panel/HBoxContainer/VBoxContainer/LabelTimeleft
 @onready var ui_cooldown: Label = $CanvasLayer/Panel/HBoxContainer/VBoxContainer/LabelCooldown
+@onready var sprite: Sprite2D = $Sprite2D
 
 var boost_available: bool = true
+const DIRECTION_TO_FRAME := {
+	Vector2.RIGHT: 0,
+	Vector2.UP: 1,
+}
+
 func _ready():
 	timer_boost.timeout.connect(_on_timer_boost_timeout)
 	timer_cooldown.timeout.connect(_on_timer_cooldown_timeout)
@@ -41,23 +47,44 @@ func _process(_delta):
 		
 func _physics_process(_delta: float):
 	
-	if Input.is_action_just_pressed("move_boost") and boost_available:
+	if Input.is_action_pressed("move_boost") and boost_available:
 		boost_available = false
 		speed = speed_boost
 		ui_speed.text = str(speed)
-		timer_boost.start(value_boost)
+		timer_boost.start(time_boost)
+		
+	if Input.is_action_just_released("move_boost"):
+		boost_available = true
+		speed = speed_normal
+		ui_speed.text = str(speed)
 		
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var desired_velocity: Vector2 = speed * direction
 	var steering: Vector2 = desired_velocity - velocity 
 	velocity += steering * drag_factor
 	move_and_slide()
-
+	
+	match direction:
+		Vector2.UP:
+			sprite.frame = 1
+			sprite.flip_v = 0
+			
+		Vector2.LEFT:
+			sprite.frame = 0
+			sprite.flip_h = 1
+			
+		Vector2.RIGHT:
+			sprite.frame = 0
+			sprite.flip_h = 0
+			
+		Vector2.DOWN:
+			sprite.frame = 1
+			sprite.flip_v = 1
 
 func _on_timer_boost_timeout() -> void:
 	speed = speed_normal
 	ui_speed.text = str(speed)
-	timer_cooldown.start(value_cooldown)
+	timer_cooldown.start(time_cooldown)
 
 
 func _on_timer_cooldown_timeout() -> void:
